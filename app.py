36d5b71ff5545ca91a682a0e1b7722e58cd06556
zoeengel <zengel1706@gmail.com>
2021-02-15 14:54:03 +0200

    added the delete button and ID

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ def init_sqlite_db():
     conn = sqlite3.connect('database.db')
     print("Opened database successfully")
 
-    conn.execute('CREATE TABLE IF NOT EXISTS students (name TEXT, addr TEXT, city TEXT, pin TEXT)')
+    conn.execute('CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, addr TEXT, '
+                 'city TEXT, pin TEXT)')
     print("Table created successfully")
     conn.close()
 
@@ -14,6 +15,7 @@ def init_sqlite_db():
 init_sqlite_db()
 
 app = Flask(__name__)
+
 
 @app.route('/')
 @app.route('/enter-new/')
@@ -42,6 +44,7 @@ def add_new_record():
             con.close()
             return render_template('result.html', msg=msg)
 
+
 @app.route('/show-records/', methods=['GET'])
 def show_redords():
     records = []
@@ -52,7 +55,24 @@ def show_redords():
             records = cur.fetchall()
     except Exception as e:
         con.rollback()
-        print("There was an error fetching results from the database.")
+        print("There was an error fetching results from the database: " + str(e))
     finally:
         con.close()
         return render_template('records.html', records=records)
+
+
+@app.route('/delete-student/<int:student_id>/', methods=["GET"])
+def delete_student(student_id):
+    msg = None
+    try:
+        with sqlite3.connect('database.db') as con:
+            cur = con.cursor()
+            cur.execute("DELETE FROM student WHERE id=" + str(student_id))
+            con.commit()
+            msg = "A record was deleted successfully from the database."
+    except Exception as e:
+        con.rollback()
+        msg = "Error occurred when deleting a student in the database: " + str(e)
+    finally:
+        con.close()
+        return render_template('delete-success.html', msg=msg)
